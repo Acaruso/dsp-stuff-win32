37b0c7c03a2ab502fa5b1c1eb524fdb6c551d2f7
Acaruso <anthonycaruso102@gmail.com>
2022-08-10 17:51:36 -0400

    stuff

--- dsp-stuff-win32/src/shared/shared_data.hpp
+++ dsp-stuff-win32/src/shared/shared_data.hpp
@@ -5,8 +5,6 @@
 class SharedData {
 public:
     moodycamel::ReaderWriterQueue<std::string> toAudio;
-    // static constexpr size_t sampleBufferSize{512};
-    // double sampleBuffer[sampleBufferSize]{0};
     size_t sampleBufferSize = 0;
     double* sampleBuffer = nullptr;
 
