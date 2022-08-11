42f8ec3d40cd390141dc222935fd4aba6adab60e
Acaruso <anthonycaruso102@gmail.com>
2022-08-11 17:09:08 -0400

    stuff

--- dsp-stuff-win32/src/audio/sample_maker.hpp
+++ dsp-stuff-win32/src/audio/sample_maker.hpp
@@ -43,7 +43,6 @@ public:
 
     double makeSample(unsigned long sampleCounter, std::string& message) {
         // TODO: don't use string for message, use enum or something
-
         if (message == "trig") {
             trig = true;
             r = getRand();
