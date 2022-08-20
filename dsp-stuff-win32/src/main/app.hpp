3611c833f9586c9556d2c95d627963e056417571
Acaruso <anthonycaruso102@gmail.com>
2022-08-20 11:18:14 -0400

    stuff

--- dsp-stuff-win32/src/main/app.hpp
+++ dsp-stuff-win32/src/main/app.hpp
@@ -73,7 +73,7 @@ public:
 
         uiRoot->pushChild(waveformElt);
 
-        BaseElt* textElt = new TextElt(&gfx, makeRectF(0, 400, 800, 200), L"some text", green);
+        BaseElt* textElt = new TextElt(&gfx, makeRectF(0, 400, 800, 200), L"some text");
 
         uiRoot->pushChild(textElt);
     }
