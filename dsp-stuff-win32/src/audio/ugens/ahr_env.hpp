50351640ff00367f07dfabff31819308bf67580d
Acaruso <anthonycaruso102@gmail.com>
2022-08-22 14:58:58 -0400

    new ugen style

--- dsp-stuff-win32/src/audio/ugens/ahr_env.hpp
+++ dsp-stuff-win32/src/audio/ugens/ahr_env.hpp
@@ -4,57 +4,6 @@
 #include "src/audio/ugens/base_ugen.hpp"
 #include "src/shared/shared_constants.hpp"
 
-// struct AHREnv {
-//     double a;
-//     double h;
-//     double r;
-
-//     unsigned attackSamps;
-//     unsigned holdSamps;
-//     unsigned releaseSamps;
-
-//     double attackDelta;
-//     double releaseDelta;
-
-//     bool on = false;
-//     double sig;
-//     unsigned timer = 0;
-
-//     void trigger(double a_, double h_, double r_) {
-//         a = a_ == 0 ? 1 : a_;
-//         h = h_ == 0 ? 1 : h_;
-//         r = r_ == 0 ? 1 : r_;
-
-//         attackSamps = mstosamps(a);
-//         holdSamps = mstosamps(h);
-//         releaseSamps = mstosamps(r);
-
-//         attackDelta = 1.0 / (double)attackSamps;
-//         releaseDelta = 1.0 / (double)releaseSamps;
-
-//         on = true;
-//         sig = 0.0;
-//         timer = 0;
-//     }
-
-//     double get(double t) {
-//         if (timer < attackSamps) {
-//             sig += attackDelta;
-//         } else if (timer < attackSamps + holdSamps) {
-//             sig = 1.0;
-//         } else if(timer < attackSamps + holdSamps + releaseSamps) {
-//             sig -= releaseDelta;
-//         } else {
-//             sig = 0.0;
-//             on = false;
-//         }
-
-//         timer += 1;
-
-//         return sig;
-//     }
-// };
-
 class AHREnv : public BaseUgen {
 public:
     double a;
