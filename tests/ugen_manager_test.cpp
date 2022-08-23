c1f01ea758fad1a93b2984d9c5103b57280eff46
Acaruso <anthonycaruso102@gmail.com>
2022-08-23 10:30:58 -0400

    stuff

---
+++ tests/ugen_manager_test.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+#include "../dsp-stuff-win32/src/audio/ugens/ugen_manager.hpp"
+
+TEST(UgenManagerTest, Basic) {
+  UgenManager ugenManager;
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
