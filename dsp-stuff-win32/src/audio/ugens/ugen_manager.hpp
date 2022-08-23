03261f2dde3a1c340a653e32b3f9b1dbe75bde22
Acaruso <anthonycaruso102@gmail.com>
2022-08-22 20:13:59 -0400

    stuff

--- dsp-stuff-win32/src/audio/ugens/ugen_manager.hpp
+++ dsp-stuff-win32/src/audio/ugens/ugen_manager.hpp
@@ -1,15 +1,26 @@
 #pragma once
 
+#include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 #include "src/audio/ugens/base_ugen.hpp"
 
+enum TopoSortStatus {
+    NOT_VISITED,
+    IN_FLIGHT,
+    VISITED
+};
+
 class UgenManager {
 public:
     std::unordered_map<int, BaseUgen*> ugens;
     std::unordered_map<int, std::unordered_set<int>> edges;
+    std::vector<int> topoSortedUgens;
+    std::unordered_map<int, TopoSortStatus> visited;
+    bool loopDetected = false;
+
     int nextId = 0;
 
     UgenManager() {}
@@ -30,5 +41,40 @@ public:
             edges[sourceId] = std::unordered_set<int>();
         }
         edges[sourceId].insert(destId);
+    }
+
+    void topoSort() {
+        topoSortedUgens.clear();
+        visited.clear();
+        loopDetected = false;
+
+        for (auto& it : ugens) {
+            visited[it.first] = NOT_VISITED;
+        }
+
+        for (auto& it : ugens) {
+            if (visited[it.first] == NOT_VISITED) {
+                topo(it.first);
+            }
+        }
+
+        std::reverse(topoSortedUgens.begin(), topoSortedUgens.end());
+    }
+
+    void topo(int id) {
+        visited[id] = IN_FLIGHT;
+
+        auto& eltEdges = edges[id];
+        for (auto& edge : eltEdges) {
+            if (visited[edge] == IN_FLIGHT) {
+                loopDetected = true;
+                return;
+            } else if (visited[edge] == NOT_VISITED) {
+                topo(edge);
+            }
+        }
+
+        visited[id] = VISITED;
+        topoSortedUgens.push_back(id);
     }
 };
