74d63a5013f0da229246e0344b50e7391da9805e
Acaruso <anthonycaruso102@gmail.com>
2022-10-15 07:21:39 -0400

    make recorder sample accurate triggered

--- dsp-stuff-win32/src/audio/ugens/recorder.hpp
+++ dsp-stuff-win32/src/audio/ugens/recorder.hpp
@@ -26,72 +26,15 @@ public:
         unsigned in0 = in[0];
         unsigned in1 = in[1];
 
-        buffer.active = READ_IN(d, in1, 0) == 1.0f;
+        for (int i = 0; i < bufferSize; ++i) {
+            buffer.active = (READ_IN(d, in1, 0) == 1.0f);
 
-        if (buffer.active) {
-            for (int i = 0; i < bufferSize; ++i) {
-                if (idx < buffer.data.size()) {
-                    buffer.data[idx] = READ_IN(d, in0, i);
-                    idx++;
-                }
+            if (buffer.active && idx < buffer.data.size()) {
+                buffer.data[idx] = READ_IN(d, in0, i);
+                ++idx;
+            } else {
+                idx = 0;
             }
-        } else {
-            idx = 0;
         }
     }
-
-    // void run(unsigned sampleCounter) {
-    //     auto& d = ugenCtx->bufferAllocator.data;
-
-    //     unsigned in0 = in[0];
-    //     unsigned in1 = in[1];
-
-    //     // buffer.active = READ_IN(d, in1, 0) == 1.0f;
-
-    //     // if (buffer.active) {
-    //     //     for (int i = 0; i < bufferSize; ++i) {
-    //     //         if (idx < buffer.data.size()) {
-    //     //             buffer.data[idx] = READ_IN(d, in0, i);
-    //     //             idx++;
-    //     //         }
-    //     //     }
-    //     // } else {
-    //     //     idx = 0;
-    //     // }
-
-    //     // for (int i = 0; i < bufferSize; ++i) {
-    //     //     buffer.active = READ_IN(d, in1, i) == 1.0f;
-
-    //     //     if (buffer.active) {
-    //     //         if (idx < buffer.data.size()) {
-    //     //             buffer.data[idx] = READ_IN(d, in0, i);
-    //     //             ++idx;
-    //     //         }
-    //     //     } else {
-    //     //         idx = 0;
-    //     //     }
-    //     // }
-
-    //     // for (int i = 0; i < bufferSize; ++i) {
-    //     //     buffer.active = READ_IN(d, in1, i) == 1.0f;
-
-    //     //     if (buffer.active && idx < buffer.data.size()) {
-    //     //         buffer.data[idx] = READ_IN(d, in0, i);
-    //     //         ++idx;
-    //     //     } else {
-    //     //         idx = 0;
-    //     //     }
-    //     // }
-        
-    //     for (int i = 0; i < bufferSize; ++i) {
-    //         buffer.active = READ_IN(d, in1, 0) == 1.0f;
-
-    //         if (buffer.active && idx < buffer.data.size()) {
-    //             buffer.data[idx] = READ_IN(d, in0, i);
-    //             ++idx;
-    //         } else {
-    //             idx = 0;
-    //         }
-    //     }
-    // }
 };
