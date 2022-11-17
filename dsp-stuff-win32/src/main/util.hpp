3b574a8e5a7b2dd0b194be5abb96fb8a63dcc70c
Acaruso <anthonycaruso102@gmail.com>
2022-11-17 08:41:34 -0500

    stuff

--- dsp-stuff-win32/src/main/util.hpp
+++ dsp-stuff-win32/src/main/util.hpp
@@ -202,19 +202,6 @@ inline std::wstring zeroExtendRight(int x, int size) {
     return ss.str();
 }
 
-// inline float round(float f, int precision) {
-//     float c = pow(10, precision);
-//     return (std::round(f * c)) / c;
-// }
-
-// inline float _round(float f, int precision) {
-//     float c = pow(10, precision);
-//     float x1 = f * c;
-//     float x2 = std::round(x1);
-//     float x3 = x2 / c;
-//     return x3;
-// }
-
 inline float _round(float f, int precision) {
     float c = pow(10, precision);
     return (std::round(f * c)) / c;
