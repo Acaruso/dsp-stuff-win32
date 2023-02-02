e63a73ace8f20a87e22bf9a397912f38b7cc6cf3
Acaruso <anthonycaruso102@gmail.com>
2023-02-02 08:29:54 -0500

    work on song2

--- dsp-stuff-win32/src/audio/ugens/song2/song2_freqs.hpp
+++ dsp-stuff-win32/src/audio/ugens/song2/song2_freqs.hpp
@@ -8,6 +8,30 @@ struct Freqs {
     float f[FREQS_SIZE];
     int size = 0;
 
+    Freqs() {}
+
+    Freqs(float f1) {
+        push(f1);
+    }
+
+    Freqs(float f1, float f2) {
+        push(f1);
+        push(f2);
+    }
+
+    Freqs(float f1, float f2, float f3) {
+        push(f1);
+        push(f2);
+        push(f3);
+    }
+
+    Freqs(float f1, float f2, float f3, float f4) {
+        push(f1);
+        push(f2);
+        push(f3);
+        push(f4);
+    }
+
     void push(float _f) {
         if (size < FREQS_SIZE) {
             f[size] = _f;
--- dsp-stuff-win32/src/audio/ugens/song2/song2_notes.hpp
+++ dsp-stuff-win32/src/audio/ugens/song2/song2_notes.hpp
@@ -24,58 +24,30 @@ public:
     ScaleType scaleType = MAJOR;
 
     Freqs makeMajorChord(int root) {
-        Freqs freqs;
-        freqs.push(noteToFreq(base + root));
-        freqs.push(noteToFreq(base + root + 4));
-        freqs.push(noteToFreq(base + root + 7));
-        freqs.push(noteToFreq(base + root - 24));
-        return freqs;
+        return Freqs(
+            noteToFreq(base + root),
+            noteToFreq(base + root + 4),
+            noteToFreq(base + root + 7),
+            noteToFreq(base + root -24)
+        );
     }
 
     Freqs makeMinorChord(int root) {
-        Freqs freqs;
-        freqs.push(noteToFreq(base + root));
-        freqs.push(noteToFreq(base + root + 3));
-        freqs.push(noteToFreq(base + root + 7));
-        freqs.push(noteToFreq(base + root - 24));
-        return freqs;
+        return Freqs(
+            noteToFreq(base + root),
+            noteToFreq(base + root + 3),
+            noteToFreq(base + root + 7),
+            noteToFreq(base + root -24)
+        );
     }
 
     Freqs makeDimChord(int root) {
-        Freqs freqs;
-        freqs.push(noteToFreq(base + root));
-        freqs.push(noteToFreq(base + root + 3));
-        freqs.push(noteToFreq(base + root + 6));
-        freqs.push(noteToFreq(base + root - 24));
-        return freqs;
-    }
-
-    Freqs makeChord(int root) {
-        if (root == 0) {
-            // c maj
-            return makeMajorChord(0);
-        } else if (root == 1) {
-            // d min
-            return makeMinorChord(2);
-        } else if (root == 2) {
-            // e min
-            return makeMinorChord(4);
-        } else if (root == 3) {
-            // f maj
-            return makeMajorChord(5);
-        } else if (root == 4) {
-            // g maj
-            return makeMajorChord(7);
-        } else if (root == 5) {
-            // a min
-            return makeMinorChord(9);
-        } else if (root == 6) {
-            // b dim
-            return makeDimChord(11);
-        } else {
-            // default
-            return makeMajorChord(0);
-        }
+        return Freqs(
+            noteToFreq(base + root),
+            noteToFreq(base + root + 3),
+            noteToFreq(base + root + 6),
+            noteToFreq(base + root -24)
+        );
     }
 };
 
