4a2fcb4916228d303be7b318c9ecbad1fbf14d2a
Acaruso <anthonycaruso102@gmail.com>
2023-02-18 14:26:52 -0500

    work on song2

--- dsp-stuff-win32/src/audio/ugens/song2/song2_advanced_synths.hpp
+++ dsp-stuff-win32/src/audio/ugens/song2/song2_advanced_synths.hpp
@@ -16,6 +16,19 @@
 
 namespace Song2 {
 
+struct AdvSynth1Params {
+    AHRData wtAhr;
+    AHRData modAhr;
+    AHRData subAhr;
+    AHRScaleData freqAhr;
+
+    float wtFreq = 0.0f;
+    float modFreq = 0.0f;
+    float subFreq = 0.0f;
+
+    float modAmount = 0.0f;
+};
+
 class AdvSynth1 : public BaseGen {
 public:
     WavetableSynth wt;
@@ -24,10 +37,13 @@ public:
     Waveshaper bitcrush;
     Waveshaper tanh;
     AdvancedSeq seq;
-
     Env freqEnv;
+    float modAmount = 1.0f;
 
     float sig = 0.0f;
+
+    std::vector<AdvSynth1Params> params;
+    int paramsCounter = 0;
 
     AdvSynth1(Wavetables* _wavetables, SeqClock* seqClock):
         wt(_wavetables->sin, AHRData{0, 10, 300}),
@@ -38,54 +54,51 @@ public:
         seq(
             seqClock,
             //1           2           3           4
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            { 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0 }
         )
     {
+        AdvSynth1Params p1;
         float h = 600;
+        p1.wtAhr     = AHRData{0, h * 0.1f, h};
+        p1.modAhr    = AHRData{0, h * 0.1f, h};
+        p1.subAhr    = AHRData{0, h, h * 0.1f};
+        p1.freqAhr   = AHRScaleData{1, 1, h, 50, 1500};
+        p1.wtFreq    = 100.0f;
+        p1.modFreq   = 50.0f;
+        p1.subFreq   = 50.0f;
+        p1.modAmount = 2.0f;
+        params.push_back(p1);
 
-        wt.setAmpEnv(AHRData {0, h * 0.1f, h});
-        mod.setAmpEnv(AHRData{0, h * 0.1f, h});
-        sub.setAmpEnv(AHRData{0, h, h * 0.1f});
-
-        freqEnv.setAhrScale(AHRScaleData{1, 1, h, 50, 1500});
-
-        wt.setFreq(100);
-        mod.setFreq(50);
-        sub.setFreq(50);
+        AdvSynth1Params p2;
+        p2.wtAhr     = AHRData{0, h * 0.1f, h * 0.2f};
+        p2.modAhr    = AHRData{0, h * 0.1f, h * 0.2f};
+        p2.subAhr    = AHRData{0, h * 0.2f, h * 0.1f};
+        p2.freqAhr   = AHRScaleData{1, 1, h * 0.2f, 100, 1500};
+        p2.wtFreq    = 100.0f;
+        p2.modFreq   = 50.0f;
+        p2.subFreq   = 50.0f;
+        p2.modAmount = 2.0f;
+        params.push_back(p2);
     }
 
-    // float get() {
-    //     sig = 0.0f;
-    //     wt.setPhaseMod(mod.get() * 1.0f);
-
-    //     // return tanh.get(
-    //     //     bitcrush.get(
-    //     //         wt.get()
-    //     //     ) * 4.0f
-    //     // );
-
-    //     // return bitcrush.get(
-    //     //     tanh.get(
-    //     //         wt.get() * 4.0f
-    //     //     )
-    //     // );
-
-    //     sig += tanh.get(wt.get() + (sub.get() * 0.06f));
-    //     sig += sub.get();
-
-    //     return sig;
-
-    //     // return wt.get();
-    // }
+    void setParams(AdvSynth1Params& p) {
+        wt.setAmpEnv(p.wtAhr);
+        mod.setAmpEnv(p.modAhr);
+        sub.setAmpEnv(p.subAhr);
+        freqEnv.setAhrScale(p.freqAhr);
+        wt.setFreq(p.wtFreq);
+        mod.setFreq(p.modFreq);
+        sub.setFreq(p.subFreq);
+        modAmount = p.modAmount;
+    }
 
     float get() {
         sig = 0.0f;
 
-        // mod.setFreq(freqEnv.get());
         wt.setFreq(freqEnv.get());
 
         wt.setPhaseMod(
-            tanh.get(mod.get() * 2.0f) * 2.0f
+            tanh.get(mod.get() * 2.0f) * modAmount
         );
 
         sig += tanh.get(wt.get() * 0.4f) * 0.6f;
@@ -97,6 +110,8 @@ public:
 
     void run() override {
         if (seq.trigger()) {
+            setParams(params[paramsCounter]);
+            paramsCounter = (paramsCounter + 1) % params.size();
             wt.trigger();
             mod.trigger();
             sub.trigger();
